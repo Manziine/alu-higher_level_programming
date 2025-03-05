994dfcf9a80267efbc5514db3c5b00856087f1e2
Ineza Manzi Arnaud <a.manziine@alusudent.com>
2025-03-04 23:55:32 -0800

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
 
-def print_last_digit(number):
-    last_digit = abs(number) % 10  # Get the last digit
-    print("{}".format(last_digit), end="")  # Print the last digit without a newline
-    return last_digit
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
 
 
-# Test cases
+# Test the function
 if __name__ == "__main__":
-    print_last_digit(98)
-    print_last_digit(0)
-    r = print_last_digit(-1024)
-    print("{}".format(r))
+    fizzbuzz()
