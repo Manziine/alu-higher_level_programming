70ff8f2b03aeb62762f5dc6a1cf58c582e9a9434
Manzi Ineza <a.manziine@alustudent.com>
2025-05-26 08:36:09 -0700

    13. Add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 
-const add = (a, b) => a + b;
+function add (a, b) {
+  return a + b;
+}
 
-module.exports.add = add;
-
-// 🔽 Test cases
-console.log(add(3, 5));    // Expected output: 8
-console.log(add(10, 20));  // Expected output: 30
-console.log(add(-5, 5));   // Expected output: 0
-console.log(add(0, 0));    // Expected output: 0
-console.log(add(1.5, 2.5));// Expected output: 4
+module.exports = { add };
---
+++ javascript-warm_up/13-main.js
@@ -0,0 +1,3 @@
+const add = require('./13-add').add;
+
+console.log(add(93, -4));  // Expected output: 89
