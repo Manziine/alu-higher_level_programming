ce55e6e55b742f510f14c9ceccec478bbc2de99c
Manzi Ineza <a.manziine@alustudent.com>
2025-05-26 04:49:40 -0700

    7. I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,4 +2,10 @@
 
 const x = Number.parseInt(process.argv[2]);
 
-console.log(Number.isNaN(x) || x <= 0 ? 'Missing number of occurrences' : 'C is fun\n'.repeat(x).trim());
+if (Number.isNaN(x) || x <= 0) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
