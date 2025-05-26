167e47f6512808a4d5502a7b8cdbbbab4689eda2
Manzi Ineza <a.manziine@alustudent.com>
2025-05-26 07:54:26 -0700

    9. Add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  return a + b;
+}
+
+const num1 = Number.parseInt(process.argv[2]);
+const num2 = Number.parseInt(process.argv[3]);
+
+console.log(Number.isNaN(num1) || Number.isNaN(num2) ? 'NaN' : add(num1, num2));
